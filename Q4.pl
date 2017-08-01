d7c0303880431760217e60587f9792da3e671d80
João Pedro <joao.costa@ccc.ufcg.edu.br>
2017-08-01 18:59:27 -0300

    Evniando Q4

---
+++ Q4.pl
@@ -0,0 +1,32 @@
+passaro(joao).
+peixe(pedro).
+minhoca(maria).
+pessoa(eu).
+gato(miau).
+amigo(eu, miau).
+
+gosta(X,Y):- 
+	passaro(X), 
+	minhoca(Y).
+gosta(X,Y):- 
+	gato(X), 
+	peixe(Y).
+gosta(X,Y):- 
+	gato(X), 
+	passaro(Y).
+amigo(X,Y):- 
+	gosta(X,Y), 
+	gosta(Y,X).
+
+naoEhUmaPessoa(X):- 
+	passaro(X); 
+	peixe(X); 
+	minhoca(X); 
+	gato(X).
+
+come(X,Y):- 
+	gato(X), 
+	gosta(X,Y), 
+	naoEhpessoa(Y).
+
+
